6902fc5c887ad8c074e8ef5b52fe8ddc51d0382b
Louie Shen <louieshen@LouiedeMacBook-Pro.local>
2025-04-23 22:24:43 -0700

    not working

--- pubtest.py
+++ pubtest.py
@@ -1,16 +1,13 @@
 import paho.mqtt.client as mqtt
+import time
 import json
-
-def on_connect(client, userdata, flags, rc):
-    client.subscribe("louieshe/feeds/250")
-
-def on_message(client, userdata, msg):
-    print(json.loads(msg.payload.decode()))
 
 client = mqtt.Client()
 client.username_pw_set("louieshe", "aio_QkNz07b4Jbyp4xAH7VIcxKl2GnIz")
-client.on_connect = on_connect
-client.on_message = on_message
 client.connect("io.adafruit.com", 1883, 60)
-client.loop_forever()
+client.loop_start()
 
+while True:
+    data = {"value": time.time()}
+    client.publish("louieshe/feeds/250", json.dumps(data))
+    time.sleep(0.5)
